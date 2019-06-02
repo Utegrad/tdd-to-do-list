fcfe41aaf6d1937d305ebf1357911dfe7ddf6af4
Matt Larsen <utegrad@gmail.com>
2019-06-01 20:13:06 -0600

    first field on Item table

--- lists/tests/test_home_page_view.py
+++ lists/tests/test_home_page_view.py
@@ -1,9 +1,9 @@
-from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
+
 from lists.views import home_page
 
-# Create your tests here.
+
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
