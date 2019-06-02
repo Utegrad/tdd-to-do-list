df6016c4fac5c1c05f5df8f33eb5995073c7f3e8
Matt Larsen <utegrad@gmail.com>
2019-06-02 12:00:57 -0600

    working on multiple lists

--- lists/tests/test_view_list_view.py
+++ lists/tests/test_view_list_view.py
@@ -6,15 +6,21 @@ from lists.models import Item, List
 class ListViewTest(TestCase):
 
     def test_view_list_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list/')
+        _list = List.objects.create(name='foo')
+        response = self.client.get(f'/lists/{_list.id}/')
         self.assertTemplateUsed(response, 'lists/list.html')
 
-    def test_displays_all_items(self):
-        _list = List.objects.create()
-        Item.objects.create(text='item 1', list=_list)
-        Item.objects.create(text='item 2', list=_list)
+    def test_displays_only_items_for_a_list(self):
+        correct_list = List.objects.create(name='a list')
+        Item.objects.create(text='item 1', list=correct_list)
+        Item.objects.create(text='item 2', list=correct_list)
+        other_list = List.objects.create(name='other list')
+        Item.objects.create(text='item a', list=other_list)
+        Item.objects.create(text='item b', list=other_list)
 
-        response = self.client.get('/lists/the-only-list/')
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'item 1')
         self.assertContains(response, 'item 2')
+        self.assertNotContains(response, 'item a')
+        self.assertNotContains(response, 'item b')
--- lists/views.py
+++ lists/views.py
@@ -6,12 +6,13 @@ def home_page(request):
     return render(request, 'lists/home.html', )
 
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    _list = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=_list)
     return render(request, 'lists/list.html', {'items': items})
 
 
 def new_list(request):
     _list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=_list)
-    return redirect('/lists/the-only-list/')
+    return redirect(f'/lists/{_list.id}/')
