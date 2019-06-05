f248bb9b8fcce8e94adc03776bb362f3a2904e68
Matt Larsen <utegrad@gmail.com>
2019-06-05 17:44:58 -0600

    smoke test for basic styling

--- functional_tests/test_navigations.py
+++ functional_tests/test_navigations.py
@@ -115,3 +115,35 @@ class NavigationTest(LiveServerTestCase):
         self.assertIn('buy milk', page_text)
 
         self.fail('WIP')
+
+
+class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver ' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(IMPLICIT_WAIT)
+
+    def tearDown(self):
+        self.browser.refresh()
+        self.browser.quit()
+
+    def test_layout_and_styling(self):
+        # smoke test to check style sheets load
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
