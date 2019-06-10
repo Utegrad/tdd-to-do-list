210c9a5c14f945942ac14f39012763561bf8370b
Matt Larsen <utegrad@gmail.com>
2019-06-10 10:48:33 -0600

    refactor settings again

--- src/django_tdd_tutorial/settings.py
+++ src/django_tdd_tutorial/settings.py
@@ -1,7 +1,21 @@
-from .base import *
+import os
+import environ
+
+ROOT_DIR = environ.Path(os.path.abspath('')) - 1  # (src/django_tdd_tutorial/settings.py - 1 = src/)
+env = environ.Env(
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(list, []),
+    INTERNAL_IPS=(list, []),
+)
+env.read_env()
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = ROOT_DIR()
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -22,7 +36,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'settings.urls'
+ROOT_URLCONF = 'django_tdd_tutorial.urls'
 
 TEMPLATES = [
     {
@@ -40,7 +54,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'settings.wsgi.application'
+WSGI_APPLICATION = 'django_tdd_tutorial.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
@@ -58,3 +72,27 @@ AUTH_PASSWORD_VALIDATORS = [
     { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
     { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (ROOT_DIR('static'), )
+
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+INTERNAL_IPS = env('INTERNAL_IPS')
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/Boise'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
