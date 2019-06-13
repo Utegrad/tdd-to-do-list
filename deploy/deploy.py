a11400aad3ef8026559ab4d98090a062d9f386b1
Matt Larsen <utegrad@gmail.com>
2019-06-12 22:50:36 -0600

    copy files, working on directories

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 from fabric import Connection
 from get_secret import get_secret
 
@@ -7,19 +8,31 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main(argz):
-    print(ROOT_DIR)
     app_secrets_name = os.environ.get('APP_SECRETS_NAME')
     region_name = os.environ.get('REGION_NAME')
     ssh_host = os.environ.get('SSH_HOST')
     ssh_username = os.environ.get('SSH_USERNAME')
-    ssh_password = os.environ.get('SSH_PASSWORD')
-    kwargs = {'password': ssh_password}
+    secrets = get_secret(app_secrets_name, region_name)
+    app_secrets = json.loads(secrets[0])
+    app_path = app_secrets['APP_PATH']
+
+    django_src = os.path.join(ROOT_DIR, 'src')
+    # relative to the django_src path
+    dirs_to_copy = ('django_tdd_tutorial', 'lists', 'templates')
+    files_to_copy = ('manage.py', )
 
     with Connection(host=ssh_host, user=ssh_username, ) as c:
-        c.run('uname -r')
+        print('Cleaning out files from application path')
+        c.run(f'find {app_path} -type f -exec rm {{}} \;')
+        for f in files_to_copy:
+            c.put(os.path.join(django_src, f), f"{app_path}/")
 
-    secrets = get_secret(app_secrets_name, region_name)
-    print(secrets)
+        for d in dirs_to_copy:
+            c.run(f'if [ -d "{app_path}/{d}" ]; then rm -Rf "{app_path}/{d}"; fi')
+            c.run(f'mkdir -p {app_path}/{d}')
+            source_path = os.path.join(django_src, d)
+            remote_path = f"{app_path}/{d}"
+            c.put(f"{source_path}", remote_path)
 
 
 if __name__ == '__main__':
