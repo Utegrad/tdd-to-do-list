29a0fca2297f6588f4b97bd070639c7272b7617f
Matt Larsen <utegrad@gmail.com>
2019-07-23 22:55:57 -0600

    unit tests for model validation of no duplicate items in a list

--- src/lists/models.py
+++ src/lists/models.py
@@ -10,5 +10,12 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default='')
+    text = models.TextField(default='', )
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('text', 'list')
+        ordering = ('id', )
+
+    def __str__(self):
+        return self.text
--- src/lists/tests/test_items_model.py
+++ src/lists/tests/test_items_model.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from django.core.exceptions import ValidationError
 
 import pytest
+from django.urls import reverse
 
 from lists.models import Item, List
 
@@ -37,31 +38,26 @@ class ListModelTest(TestCase):
         self.assertEqual(second_saved_item.text, second_item_text)
         self.assertEqual(second_saved_item.list, _list)
 
+    @pytest.mark.django_db
+    def test_get_absolute_url(self):
+        _list = List.objects.create()
+        self.assertEqual(_list.get_absolute_url(), reverse('lists:view_list', args=[_list.id, ]))
+
 
 class ItemModelTest(TestCase):
+
     @pytest.mark.django_db
-    def test_saving_and_retrieving_items(self):
-        first_item_text = 'The first (ever) list item'
-        second_item_text = 'Item the second'
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
 
-        _list = List()
-        _list.save()
-
-        first_item = Item(list=_list)
-        first_item.text = first_item_text
-        first_item.save()
-
-        second_item = Item(list=_list)
-        second_item.text = second_item_text
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertGreaterEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, first_item_text)
-        self.assertEqual(second_saved_item.text, second_item_text)
+    @pytest.mark.django_db
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
 
     @pytest.mark.django_db
     def test_cannot_save_empty_list_items(self):
@@ -70,3 +66,41 @@ class ItemModelTest(TestCase):
         with self.assertRaises(ValidationError):
             item.full_clean()
             item.save()
+
+    @pytest.mark.django_db
+    def test_duplicate_items_in_same_list_are_invalid(self):
+        item_text = 'blah'
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text=item_text)
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text=item_text)
+            item.full_clean()
+
+    @pytest.mark.django_db
+    def test_CAN_save_duplicate_item_to_seperate_list(self):
+        item_text = 'blah'
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text=item_text)
+        item = Item(list=list2, text=item_text)
+        item.full_clean()  # should not raise
+
+    @pytest.mark.django_db
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='_a')
+        item2 = Item.objects.create(list=list1, text='_b')
+        item3 = Item.objects.create(list=list1, text='_c')
+        self.assertEqual(
+            list(Item.objects.filter(list__pk=list1.id)),
+            [item1, item2, item3]
+        )
+        self.assertEqual(
+            list(list1.item_set.all()),  # another way to get the queryset based on the ForeignKey relationship
+            [item1, item2, item3]
+        )
+
+    @pytest.mark.django_db
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
