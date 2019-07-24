f0f75eeed8a1cec15185a3e5bd6b42ce93317306
Matt Larsen <utegrad@gmail.com>
2019-07-23 18:50:22 -0600

    simplify views by using form to save items

--- src/lists/views.py
+++ src/lists/views.py
@@ -13,11 +13,10 @@ def home_page(request):
 def view_list(request, list_id):
     _list = List.objects.get(id=list_id)
     form = ItemForm()
-    error = None
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=_list)
+            form.save(for_list=_list)
             return redirect(_list)
     return render(request, 'lists/list.html', {'list': _list, 'form': form})
 
@@ -26,7 +25,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists/home.html', {'form': form})
