a06af5421b3f7b7e476a11d1be58ba02fe991631
Matt Larsen <utegrad@gmail.com>
2019-08-11 16:31:38 -0600

    change database fields

--- src/lists/models.py
+++ src/lists/models.py
@@ -3,15 +3,15 @@ from django.urls import reverse
 
 
 class List(models.Model):
-    name = models.TextField(max_length=128, default='')
+    name = models.CharField(max_length=128, default='')
 
     def get_absolute_url(self):
         return reverse('lists:view_list', args=[self.id])
 
 
 class Item(models.Model):
-    text = models.TextField(default='', )
-    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+    text = models.CharField(max_length=128, blank=False, )
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('text', 'list')
